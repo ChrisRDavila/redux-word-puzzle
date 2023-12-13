e06a9855b52189ed20b58801e4d739121722f1e5
ChrisRDavila <crossdavila@gmail.com>
2023-12-12 17:00:39 -0800

    WIP
    Co-authored-by: Monica Barboza <MonBoza@users.noreply.github.com>

--- src/components/RandomWord.jsx
+++ src/components/RandomWord.jsx
@@ -1,19 +1,35 @@
 import PropTypes from "prop-types";
+import React from "react";
 
-const words = [
+const guessWords = [
   adore, amore, amour, diety, flame, heart, honey 
 ]
 
-function getWord(words) {
+
+function handleStartGame (event) {
+  event.preventDefault();
+
+  props.getWord() {
+  const words = guessWords;
   const word = words[Math.floor(Math.random() * words.length )]
+  return word;
+}
 }
 function RandomWord(props){
   return (
-  {props.word}
-  );
+  <div>
+  <form whenStartClicked={props.handleStartGame}>
+    <button type="submit">Start Game</button>
+  </form>
+  <p>{props.word}</p> 
+  {/* doesnt need to be visible passing prop */}
+  </div>
+  )
 }
+
 RandomWord.propTypes = {
   word: PropTypes.string,
+  getWord: PropTypes.func
 }
 
 export default RandomWord;
